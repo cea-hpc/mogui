cf5837286543980d474acb97af8a22f76f9f0614
Xavier Delaruelle <xavier.delaruelle@cea.fr>
2024-02-23 20:18:31 +0100

    Set this project under GPLv2+ terms
    Use GPL-v2-or-later licence for MoGui project. Add licence file and add
    short licence header in code source files.

--- lib/module.py
+++ lib/module.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# MoGui: Gui frontend for module
-# Author: A. Cedeyn
+
+# LIB/MODULE, Python object interface to module
+# Copyright (C) 2011-2024 Aurelien Cedeyn
 #
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+##########################################################################
 
 import os, string
 from subprocess import Popen, PIPE
